9f35ead0e909f45f72a904e3a625a91860b2912b
Seamus James <seamus@blockreg.io>
2021-11-19 23:03:19 -0800

    Refactored RemoteStorage to be a generic, reusable solution.

---
+++ src/contracts/Storable.sol
@@ -0,0 +1,96 @@
+//SPDX-License-Identifier: CC-BY-NC-2.0
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+
+abstract contract Storable is ChainlinkClient {
+	using Chainlink for Chainlink.Request;
+	uint256 constant private ORACLE_PAYMENT = 1 * LINK_DIVISIBILITY;
+	bytes32 internal jobId;
+
+	struct StorableData {
+		mapping(string => string) stringValues;
+		mapping(string => uint) uintValues;
+		string[] stringKeys;
+		string[] uintKeys;
+	}
+	
+	StorableData _data;
+	bytes32 storageCid;
+
+	constructor(address _oracle, string memory _jobId) {
+		setChainlinkToken(0xa36085F69e2889c224210F603D836748e7dC0088); //Kovan
+		setChainlinkOracle(_oracle); 
+		jobId = _stringToBytes32(_jobId);
+	}
+
+	/** 
+	* @notice When the oracle fulfills the request, this callback will be invoked. 
+	* @param _requestId required bytes32 
+	* @param any should match the ABI in the job spec
+	* @dev required modifier: recordChainlinkFulfillment(_requestId) 
+	*/
+	// function onDataStored(bytes32 _requestId) virtual public;
+
+	/**
+	* @notice Queue string data to be stored
+	* @param _key string key to add
+	* @param _value string value to add
+	*/
+	function _addStorableString(string memory _key, string memory _value) internal {
+		_data.stringKeys.push(_key);
+		_data.stringValues[_key] = _value;
+	}
+
+	/**
+	* @notice Queue uint data to be stored
+	* @param _key string key to add
+	* @param _value uint value to add
+	*/
+	function _addStorableUint(string memory _key, uint _value) internal {
+		_data.uintKeys.push(_key);
+		_data.uintValues[_key] = _value;
+	}
+
+	/**
+	* @notice Store the data on IPFS
+	* @param _callbackFunctionSignature bytes3 the .selector of the function the oracle will call when it fulfills
+	*/
+	function _storeData(bytes4 _callbackFunctionSignature) internal {
+		Chainlink.Request memory req = buildChainlinkRequest(jobId, address(this), _callbackFunctionSignature);
+		for(uint i=0; i<_data.stringKeys.length; i++) {
+			req.add(_data.stringKeys[i], _data.stringValues[_data.stringKeys[i]]);
+			// delete(_data.stringValues[_data.stringKeys[i]]);
+		}
+		for(uint j=0; j<_data.uintKeys.length; j++) {
+			req.addUint(_data.uintKeys[j], _data.uintValues[_data.uintKeys[j]]);
+			// delete(_data.uintValues[_data.uintKeys[j]]);
+		}
+		requestOracleData(req, ORACLE_PAYMENT);
+	}
+
+	/**
+	* @notice Convert from bytes32 to string 
+	* @param source bytes32 
+	* @return string 
+	*/
+	function _bytes32ToString(bytes32 source) private pure returns (string memory) {
+		return string(abi.encodePacked(source));
+	}
+
+	/**
+	* @notice Convert from string to bytes32
+	* @param source string 
+	* @return result bytes32
+	*/
+	function _stringToBytes32(string memory source) private pure returns (bytes32 result) {
+		bytes memory tempEmptyStringTest = bytes(source);
+		if (tempEmptyStringTest.length == 0) {
+			return 0x0;
+		}
+
+		assembly { // solhint-disable-line no-inline-assembly
+			result := mload(add(source, 32))
+		}
+	}
+}
